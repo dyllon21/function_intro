1f8f7e73211eb357e295e6416749ea7880cfa7d3
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-08 08:57:37 -0500

    added regNumbers and called them to get results through assert

---
+++ countRegNumber.js
@@ -0,0 +1,11 @@
+var countRegNunber = function(Regumber) 
+
+
+var CA 182736,CY 523519,CJ 812328 = function (Regnumber){
+  return 'CA 182736,CY 523519,CJ 812328'
+
+
+
+
+}
+assert.equal(countRegNunber('CA 182736,CY 523519,CJ 812328'))
