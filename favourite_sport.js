7223af177ec01b650aab99234032b0b6ff90606f
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-08 09:19:08 -0500

    have declared a variable favSport of an individual and went and called it to check if wheather it is his favSport

---
+++ favourite_sport.js
@@ -0,0 +1,8 @@
+var name = "David Blanket";
+var favSport = function() {
+    var favouriteSport = "Soccer";
+    console.log(name + "'s favourite sport is " + favouriteSport);
+};
+//favouriteSport()
+favSport()
+console.log(favouriteSport);
