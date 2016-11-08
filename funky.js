cdf65a594e04f000fdb343f3f364eb05b5368b39
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-08 09:47:06 -0500

    have created a function called multiplyThem which relates to two parameters and after executing it,it gave results

---
+++ funky.js
@@ -0,0 +1,10 @@
+var multiplyThem = function(paraOne, paraTwo) {
+    var result = paraOne * paraTwo;
+    return result
+};
+
+var ff = multiplyThem(2, 3);
+console.log(ff);
+
+var ff2 = multiplyThem(15, 5)
+console.log(ff2);
