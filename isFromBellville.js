2874333d21b9e4104b8e02907cc3aa908d7458bd
dyllon21 <isaacsdyllon123@gmail.com>
2016-11-10 05:34:16 -0500

    Delete isFromBellville.js

--- isFromBellville.js
+++
@@ -1,8 +0,0 @@
-var isFromBellville = function(true,false){
- var Registration = ("startsWith CY");
- var Registration = ("endsWith CY");
-return result
-};
-isFromBellville = "startsWith CY"
-isFromBellville = "endsWith CY"
-console.log("Registration");
