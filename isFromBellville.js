cb858eb11d6f4ed4c8f4b01c0f329580c88521eb
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-10 12:28:03 -0500

    used the split function and the length function to evaluate the registration numbers

---
+++ isFromBellville.js
@@ -0,0 +1,8 @@
+var isFromBellville = function(true,false){
+ var Registration = ("startsWith CY");
+ var Registration = ("endsWith CY");
+return result
+};
+isFromBellville = "startsWith CY"
+isFromBellville = "endsWith CY"
+console.log("Registration");
